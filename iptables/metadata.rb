797ad8d7e4518c8cdae0d3ec9cf34ae2fc54c8f2
pli888 <peter@gigasciencejournal.com>
2016-07-15 18:04:55 +0800

    Add iptables cookbook

---
+++ iptables/metadata.rb
@@ -0,0 +1,17 @@
+name 'iptables'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs the iptables daemon and provides a LWRP for managing rules'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '2.2.0'
+
+recipe 'default', 'Installs iptables and sets up .d style config directory of iptables rules'
+recipe 'disabled', 'Disables iptables'
+
+%w(redhat centos debian ubuntu amazon scientific oracle amazon zlinux).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/iptables' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/iptables/issues' if respond_to?(:issues_url)
