177cca08c99346c2d6af98dc3c299d84a98ee935
pli888 <peter@gigasciencejournal.com>
2016-11-09 11:04:55 +0800

    Add mysql2_chef_gem

---
+++ mysql2_chef_gem/metadata.rb
@@ -0,0 +1,23 @@
+name 'mysql2_chef_gem'
+maintainer 'Nicolas Blanc'
+maintainer_email 'sinfomicien@gmail.com'
+license 'Apache 2.0'
+description 'Provides the mysql2_chef_gem resource'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.1.0'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
+supports 'oracle'
+
+depends 'build-essential'
+depends 'mysql', '>= 6.0'
+depends 'mariadb'
+
+source_url 'https://github.com/sinfomicien/mysql2_chef_gem' if respond_to?(:source_url)
+issues_url 'https://github.com/sinfomicien/mysql2_chef_gem/issues' if respond_to?(:issues_url)
