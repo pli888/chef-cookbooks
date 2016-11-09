804563016d0e2aac912281921188c736cff806fc
pli888 <peter@gigasciencejournal.com>
2016-11-09 11:06:56 +0800

    Update nginx cookbook

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,30 +1,6 @@
-case node[:platform_family]
-when 'rhel'
-    include_recipe 'yum::epel'
-end
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
 
-# Use upstream repo if a version is specified
-if node[:nginx][:version]
-    case node[:platform_family]
-    when 'rhel'
-        include_recipe 'nginx::upstream_redhat'
-    when 'debian'
-        include_recipe 'nginx::upstream_debian'
-    end
-
-end
-
-package 'nginx' do
-    # :install (default action) will not upgrade to the newest version if Nginx
-    # is already installed. :upgrade may cause some subtle problems but I guess
-    # it's OK for now.
-    action :upgrade
-    if node[:nginx][:version]
-        version node[:nginx][:version]
-    end
-end
-
-include_recipe 'nginx::commons'
-service 'nginx' do
-    action [:enable, :start]
-end
+# default recipe is intentionally left blank
