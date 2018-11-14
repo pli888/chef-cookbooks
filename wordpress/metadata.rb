8e23a694871bff5dbea6aed22fef585955e0ec5a
Peter Li <peter@gigasciencejournal.com>
2018-11-14 14:40:49 +0800

    Remove version restriction on package dependencies for WordPress

--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -13,16 +13,16 @@ recipe "WordPress::languages", "Install WordPress translation files"
   depends cb
 end
 
-depends "apache2", ">= 2.0.0"
-depends "database", ">= 1.6.0"
-depends "mysql", ">= 6.0"
-depends "mysql2_chef_gem", "~> 1.0.1"
+depends "apache2"
+depends "database"
+depends "mysql"
+depends "mysql2_chef_gem"
 # depends "build-essential"
-depends "iis", ">= 1.6.2"
-depends "tar", ">= 0.3.1"
-depends "nginx", "~> 2.7.4"
-depends "php-fpm", "~> 0.6.10"
-depends 'selinux', '~> 0.7'
+depends "iis"
+depends "tar"
+depends "nginx"
+depends "php-fpm"
+depends "selinux"
 
 %w{ debian ubuntu windows centos redhat scientific oracle }.each do |os|
   supports os
